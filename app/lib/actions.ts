8f0d48752d181feb8f8cf4c44be55276c5df53b9
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2024-11-01 15:37:18 +0000

    creating an invoice

---
+++ app/lib/actions.ts
@@ -0,0 +1,41 @@
+"use server";
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+
+const InvoiceDefinition = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const FormSchema = InvoiceDefinition.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+
+    const {
+        amount,
+        customerId,
+        status
+    } = FormSchema.parse({
+        'customerId': formData.get('customerId'),
+        'amount': formData.get('amount'),
+        'status': formData.get('status')
+    });
+
+    const date = new Date().toISOString().split("T")[0];
+
+    await sql`
+        INSERT into invoices(customer_id, amount, status, date) values (${customerId}, ${amount * 100}, ${status}, ${date})
+    `;
+
+   
+
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+
+}
